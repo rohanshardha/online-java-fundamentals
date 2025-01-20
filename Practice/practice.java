61b4bdca6e294e7f5bbdd0f051798d84673d7aeb
rohanshardha <rpshardha15@gmail.com>
2025-01-20 14:51:59 -0500

    Adding a practice folder and file

---
+++ Practice/practice.java
@@ -0,0 +1,13 @@
+class practice {
+    public static void main(String[] args) {
+      // example 
+      double i = 1289L;
+      System.out.println("i = " + i);
+  
+      // now your turn! write your code below this line
+  
+  
+     
+      // be sure to keep your code above this line
+    }
+  }
